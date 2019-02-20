ca531e688aa8065783304d0e608df1439ec9f05d
Jared Graham <grahamjared5@gmail.com>
2019-02-19 20:31:35 -0500

    improved implicit operators/ copy constructions

--- ecs/component.h
+++ ecs/component.h
@@ -2,10 +2,20 @@
 
 namespace ecs
 {
-    template <class T, class empty>
-    class make_component : public T
+    template <typename T, class empty>
+    struct make_component : public T
     {
-    public:
         using T::T;
+        make_component() = default;
+        make_component(const T & parent)
+            : T(parent)
+        {
+        }
+
+        make_component<T, empty> & operator=(const T & parent)
+        {
+            T::operator=(parent);
+            return *this;
+        }
     };
 }
