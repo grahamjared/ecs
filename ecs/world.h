39954dfcb9d78f602377d0bff2c1994f78e6e63c
Jared Graham <grahamjared5@gmail.com>
2019-04-27 16:35:20 -0400

    Move definitions into separate .inl file

--- ecs/world.h
+++ ecs/world.h
@@ -90,173 +90,66 @@ namespace ecs
         // index <I>
         ////////////////////////////////////////////////////////////
         template <class T>
-        inline auto & get_component()
-        {
-            return MatchingField<0, T, vectors_t, vector_of_t<0, T>::value>::get(m_components);
-        }
+		inline auto & get_component();
 
         template <class T>
-        inline const auto & get_component() const
-        {
-            return MatchingField<0, T, vectors_t, vector_of_t<0, T>::value>::get(m_components);
-        }
+		inline const auto & get_component() const;
 
         ////////////////////////////////////////////////////////////
         // get an entity's component based on type
         ////////////////////////////////////////////////////////////
         template <class T>
-        inline auto & get(ecs::entity entity)
-        {
-            return MatchingField<0, T, vectors_t, vector_of_t<0, T>::value>::get(m_components)[entity];
-        }
+		inline auto & get(ecs::entity entity);
 
         template <class T>
-        inline const auto & get(ecs::entity entity) const
-        {
-            return MatchingField<0, T, vectors_t, vector_of_t<0, T>::value>::get(m_components)[entity];
-        }
+		inline const auto & get(ecs::entity entity) const;
 
-        inline auto & keychain(ecs::entity entity)
-        {
-            return get<ecs::keychain<system_list>>(entity);
-        }
+		inline auto & keychain(ecs::entity entity);
 
-        inline const auto & keychain(ecs::entity entity) const
-        {
-            return get<ecs::keychain<system_list>>(entity);
-        }
+		inline const auto & keychain(ecs::entity entity) const;
 
         ////////////////////////////////////////////////////////////
         // First tries to find a deleted entity (an entity with all keys empty) and repurpose that as a new entity
         // If that fails, it pushes back all components and returns the newly allocated entity
         ////////////////////////////////////////////////////////////
-        inline entity create()
-        {
-            for (entity entity = 0; entity < size(); ++entity)
-                if (get<ecs::keychain<system_list>>(entity).empty())
-                    return entity;
-
-            push_all();
-            return size() - 1;
-        }
+		inline entity create();
 
         ////////////////////////////////////////////////////////////
         // All destroying an entity does is clear its keychain
         // This marks it as destroyed, so it can be repurposed without reallocating more memory
         ////////////////////////////////////////////////////////////
-        inline void destroy(entity entity) noexcept
-        {
-            get<ecs::keychain<system_list>>(entity).clear();
-        }
+		inline void destroy(entity entity) noexcept;
 
         ////////////////////////////////////////////////////////////
         // Tests if a entity is within the bounds of the world
         ////////////////////////////////////////////////////////////
-        inline bool valid(entity entity) const noexcept
-        {
-            return (entity > -1 && entity < static_cast<int>(size()));
-        }
+		inline bool valid(entity entity) const noexcept;
 
         ////////////////////////////////////////////////////////////
         // Tests if an entity has at least one non-empty key
         // (aka still being used by a system)
         ////////////////////////////////////////////////////////////
-        inline bool exists(entity entity) const noexcept
-        {
-            if (!valid(entity))
-                return false;
-            else
-                return !get<ecs::keychain<system_list>>(entity).empty();
-        }
+		inline bool exists(entity entity) const noexcept;
 
         ////////////////////////////////////////////////////////////
         // Below are functions identical to std::vector:: functions
         // They work exactly the same, but operate on all vectors
         ////////////////////////////////////////////////////////////
 
-        inline size_t capacity() noexcept
-        {
-            return get_component<ecs::keychain<system_list>>().capacity();
-        }
-
-        inline void clear() noexcept
-        {
-            for_each_in_tuple(m_components, [] (auto & vector)
-            {
-                vector.clear();
-            });
-        }
-
-        inline void erase(entity entity)
-        {
-            for_each_in_tuple(m_components, [entity] (auto & vector)
-            {
-                vector.erase(vector.begin() + entity);
-            });
-        }
-
-        inline void erase(entity first, entity last)
-        {
-            for_each_in_tuple(m_components, [first, last] (auto & vector)
-            {
-                vector.erase(vector.begin() + first, vector.begin() + last);
-            });
-        }
-
-        inline size_t max_size() const noexcept
-        {
-            return get_component<ecs::keychain<system_list>>().max_size();
-        }
-
-        inline void reserve(size_t new_cap)
-        {
-            for_each_in_tuple(m_components, [new_cap] (auto & vector)
-            {
-                vector.reserve(new_cap);
-            });
-        }
-
-        inline void resize(size_t count)
-        {
-            for_each_in_tuple(m_components, [count] (auto & vector)
-            {
-                vector.resize(count);
-            });
-        }
-
-        inline void shrink_to_fit()
-        {
-            for_each_in_tuple(m_components, [] (auto & vector)
-            {
-                vector.shrink_to_fit();
-            });
-        }
-
-        inline size_t size() const noexcept
-        {
-            return get_component<ecs::keychain<system_list>>().size();
-        }
-
-
-        inline entity copy(ecs::entity entity)
-        {
-            auto copy = this->create();
-            for_each_in_tuple(m_components, [copy, entity] (auto & vector)
-            {
-                vector[copy] = vector[entity];
-            });
-
-            return copy;
-        }
+		inline size_t capacity() noexcept;
+		inline void clear() noexcept;
+		inline void erase(entity entity);
+		inline void erase(entity first, entity last);        
+		inline size_t max_size() const noexcept;
+		inline void reserve(size_t new_cap);
+		inline void resize(size_t count);
+		inline void shrink_to_fit();
+		inline size_t size() const noexcept;
+		inline entity copy(ecs::entity entity);
 
     private:
-        inline void push_all()
-        {
-            for_each_in_tuple(m_components, [] (auto & vector)
-            {
-                using T = std::decay_t<decltype(vector)>;
-                vector.push_back(T::value_type());
-            });
-        }
+		inline void push_all();
     };
 }
+
+#include "world.inl"
